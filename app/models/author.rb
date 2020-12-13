f3ee563d9c1208c6915d283a0f1d38451f74acd6
yani82 <yani.iman1@gmail.com>
2020-12-13 01:05:30 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates_presence_of :title 
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :content, length: { minimum: 100 }
 end
